5dee89b287257176c7a082141c4e5f81a46a8d5c
johnniesimpson <johnniesimpson@outlook.com>
2021-09-06 15:42:51 -0400

    Beginnings of state to capture user inputs.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,22 +8,18 @@ dotenv.config();
 
 export default function Main(props) {
 
-    // const [values, setValues ] = useState(initialValues);
-
-    // const [validated, setValidated] = useState(false);
-
-    const [title] = useState();
+    const [title, setTitle] = useState("");
 
     function buildQueryURL(e) {
         e.preventDefault();
         var key = process.env.API
-        // var searchTerm =  e.target.value;
         var queryURL = "https://www.googleapis.com/books/v1/volumes?q=harry+potter";
         axios.get(queryURL).then((data) => {
             var searchReturn = data.data.items;
             console.log(searchReturn[0].volumeInfo.title)
           })
         return (queryURL)
+        
     }
  
     return (
@@ -33,7 +29,8 @@ export default function Main(props) {
             <div className="container-fluid">
               <a className="navbar-brand" href="/">Google Books Search Utility</a>
                 <form className="d-flex" onSubmit={buildQueryURL}>
-                <input className="form-control me-2" id="search-term" type="text" placeholder="Search" aria-label="Book or Author Search"></input>
+                <input className="form-control" id="search-term" type="text" placeholder="Search" aria-label="Book or Author Search"
+               value={title} onChange={(e) => setTitle(e.target.value)}></input>
                 <button className="btn btn-outline-success" id="run-search" type="submit">Search</button>
                 </form>
             </div>
@@ -41,6 +38,7 @@ export default function Main(props) {
        
         <div className="container">
             <div className="row" id="main">
+                <p>{title}</p>
                 <div className="col-4" id="pic"></div>
                 <img id="book-pic"></img>
             </div>
